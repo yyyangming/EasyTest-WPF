9f7fcac975feaf267ef664d023f3a533726fa34b
yy <1392261384@qq.com>
2021-10-14 18:33:18 +0800

    修改1

--- StyleTest.xaml.cs
+++ StyleTest.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,8 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Windows.Media.Animation;
-using System.Windows.Threading;
 
 namespace Test
 {
@@ -22,32 +21,7 @@ namespace Test
     {
         public StyleTest()
         {
-
-        }
-        private void btnSure_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-
-        }
-
-
-        DispatcherTimer tm = new DispatcherTimer();
-        void tm_Tick(object sender, EventArgs e)
-        {
-            this.Visibility = Visibility.Collapsed;
-            this.tm.Stop();
-            this.Close();
-        }
-
-        private void button1_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            tm.Tick += new EventHandler(tm_Tick);
-            tm.Interval = TimeSpan.FromSeconds(0.2);
-            tm.Start();
+            InitializeComponent();
         }
     }
 }
