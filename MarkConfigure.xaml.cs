f984852b718e1bf6598b608f7764de9da7607f99
yy <1392261384@qq.com>
2021-10-28 18:14:33 +0800

    1

--- MarkConfigure.xaml.cs
+++ MarkConfigure.xaml.cs
@@ -19,6 +19,7 @@ namespace Test
     /// </summary>
     public partial class MarkConfigure : Window
     {
+
         public MarkConfigure()
         {
             InitializeComponent();
@@ -32,21 +33,75 @@ namespace Test
             base.OnClosing(e);
         }
 
-        private void btnShowJog_Click(object sender, RoutedEventArgs e)
-        {
-            JogPage.Visibility = Visibility.Visible;
-        }
+        //private void btnShowJog_Click(object sender, RoutedEventArgs e)
+        //{
+        //    JogPage.Visibility = Visibility.Visible;
+        //}
 
         private void btnOpenJog_Click(object sender, RoutedEventArgs e)
         {
-            if (markConfigure.Width == 500)
+            if (markConfigure.Width > 780 && markConfigure.Width < 820)
             {
-                markConfigure.Width = 800;
+                markConfigure.Width = markConfigure.Width - 300;
                 //JogVersion2 jogVersion2 = new JogVersion2();
                 //JogPage.Source = new Uri(Convert.ToString( jogVersion2));
             }
-            else
-                markConfigure.Width = 500;
+            else if (markConfigure.Width == 500|| markConfigure.Width < 200)
+            {
+                markConfigure.Width += 300;
+            }
+            else if (markConfigure.Width > 480 && markConfigure.Width < 520)
+            {
+                markConfigure.Width += 300;
+            }
+            else if (markConfigure.Width > 420&& markConfigure.Width < 440)
+            {
+                markConfigure.Width = markConfigure.Width - 300;
+            }
+
         }
+
+        private void btnOpenView_Click(object sender, RoutedEventArgs e)
+        {
+            if (markConfigure.Width > 780 && markConfigure.Width <820)
+            {
+                markConfigure.Width = markConfigure.Width - 370;
+                markconfigure.Margin = new Thickness    (-370, 0, 0, 0);
+            }
+            else if ( markConfigure.Width < 200)
+            {
+                markConfigure.Width += 370;
+                markconfigure.Margin = new Thickness(0, 0, 0, 0);
+            }
+            else if (markConfigure.Width < 440 && markConfigure.Width > 420)
+            {
+                markConfigure.Width += 370;
+                markconfigure.Margin = new Thickness(0, 0, 0, 0);
+            }
+            else if (markConfigure.Width >480&& markConfigure.Width < 520)
+            {
+                markConfigure.Width = markConfigure.Width - 370;
+                markconfigure.Margin = new Thickness(-370, 0, 0, 0);
+            }
+
+        }
+
+        //private void btnOpenView_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (MarkUserView.Width == 370)
+        //    {
+        //        markConfigure.Width=430;
+        //        MarkUserView.Width = 0;
+        //    }
+        //    else
+        //    {
+        //        MarkUserView.Width = 370;
+        //        markConfigure.Width = 800;
+        //    }
+
+        //}
+
+
     }
+
 }
