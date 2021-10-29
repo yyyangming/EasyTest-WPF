36bf0fa405a6b6c2e806c6eafe07f1db33c5a54d
yy <1392261384@qq.com>
2021-10-29 10:29:36 +0800

    1

--- text.xaml.cs
+++ text.xaml.cs
@@ -26,34 +26,34 @@ namespace exSplitter
         public MainWindow()
         {
             InitializeComponent();
-            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            //this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
         }
 
-        void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            //获取GridSplitterr的cotrolTemplate中的按钮btn，必须在Loaded之后才能获取到
-            Button btnGrdSplitter = gsSplitterr.Template.FindName("btnExpend", gsSplitterr) as Button;
-            if (btnGrdSplitter != null)
-                btnGrdSplitter.Click += new RoutedEventHandler(btnGrdSplitter_Click);
-        }
+        //void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    //获取GridSplitterr的cotrolTemplate中的按钮btn，必须在Loaded之后才能获取到
+        //    Button btnGrdSplitter = gsSplitterr.Template.FindName("btnExpend", gsSplitterr) as Button;
+        //    if (btnGrdSplitter != null)
+        //        btnGrdSplitter.Click += new RoutedEventHandler(btnGrdSplitter_Click);
+        //}
 
 
-        void btnGrdSplitter_Click(object sender, RoutedEventArgs e)
-        {
-            GridLength temp = grdWorkbench.ColumnDefinitions[0].Width;
-            GridLength def = new GridLength();
-            if (temp.Equals(def))
-            {
-                //恢复
-                grdWorkbench.ColumnDefinitions[0].Width = m_WidthCache;
-            }
-            else
-            {
-                //折叠
-                m_WidthCache = grdWorkbench.ColumnDefinitions[0].Width;
-                grdWorkbench.ColumnDefinitions[0].Width = def;
-            }
-        }
+        //void btnGrdSplitter_Click(object sender, RoutedEventArgs e)
+        //{
+        //    GridLength temp = grdWorkbench.ColumnDefinitions[0].Width;
+        //    GridLength def = new GridLength();
+        //    if (temp.Equals(def))
+        //    {
+        //        //恢复
+        //        grdWorkbench.ColumnDefinitions[0].Width = m_WidthCache;
+        //    }
+        //    else
+        //    {
+        //        //折叠
+        //        m_WidthCache = grdWorkbench.ColumnDefinitions[0].Width;
+        //        grdWorkbench.ColumnDefinitions[0].Width = def;
+        //    }
+        //}
     }
 }
 
